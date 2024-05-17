4731f3db527479f7056e1a17f44b5acb64a40b1a
GianniCbd <giannicabiddu@gmail.com>
2024-05-17 16:55:28 +0200

    @Annotation personalizzata

--- src/main/java/org/example/annotation/GameInfo.java
+++ src/main/java/org/example/annotation/GameInfo.java
@@ -1,7 +1,12 @@
 package org.example.annotation;
 
-public class GameInfo {
+import java.lang.annotation.*;
 
-
-
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface GameInfo {
+    String levelName();
+    String difficulty();
+    String creator();
 }
--- src/main/java/org/example/annotation/PersAnnotation.java
+++ src/main/java/org/example/annotation/PersAnnotation.java
@@ -1,10 +1,29 @@
 package org.example.annotation;
 
+import java.lang.reflect.Method;
+
 public class PersAnnotation {
 
     public static void main(String[] args) {
 
+Class<ForestLevel> obj = ForestLevel.class;
 
+if( obj.isAnnotationPresent(GameInfo.class)){
+    GameInfo gameInfo = obj.getAnnotation(GameInfo.class);
+    System.out.println("Level Name: " + gameInfo.levelName());
+    System.out.println("Difficulty: " + gameInfo.difficulty());
+    System.out.println("Creator: " + gameInfo.creator());
+}
+
+        Method[] methods = obj.getDeclaredMethods();
+        for(Method method : methods){
+            if(method.isAnnotationPresent(GameInfo.class)){
+        GameInfo gameInfo = method.getAnnotation(GameInfo.class);
+                System.out.println("Level Name: " + gameInfo.levelName());
+                System.out.println("Difficulty: " + gameInfo.difficulty());
+                System.out.println("Creator: " + gameInfo.creator());
+    }
+}
 
     }
 }
